1d6a5ce0334dd1058b56fce42709e2340768fc4b
ryankkzy <ryan37370308@gmail.com>
2024-11-24 19:25:46 -0300

    subindo

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const flashcards = document.querySelectorAll('.flashcard');
+
+
+flashcards.forEach(card => {
+    card.addEventListener('click', () => {
+        card.classList.toggle('flipped');
+    });
+});
